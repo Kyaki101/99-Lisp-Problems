ce4ae2151aae9fee39937b470769681c6b15f220
kyaki <emanuelrs101@gmail.com>
2024-02-21 18:41:02 -0600

    Completed problem 4

---
+++ P04.scm
@@ -0,0 +1,9 @@
+;Find the number of elements of a list
+
+(define num-elements(lambda(x)
+                        (cond((null? (cdr x))
+                             1)
+                            (else(+ 1 (num-elements (cdr x)))))))
+
+
+
